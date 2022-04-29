899b428ff0d3ccb7c947e742aa384c2e96f718dd
Aleksej <Aleksejmocny@gmail.com>
2022-04-29 12:31:59 +0200

    fix: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -19,11 +19,12 @@ const HALF_LIFE_PERIOD = 5730;
  */
  function dateSample(sampleActivity) {
   let res;
-if( +sampleActivity != 'number') {
-  return false
-} else res = (Math.log(MODERN_ACTIVITY / sampleActivity))/(0.693/HALF_LIFE_PERIOD)
-let result = Math.ceil(res )
+  if (typeof(sampleActivity) == 'string' && (+sampleActivity > 0) ) {
+res = (Math.log(MODERN_ACTIVITY / sampleActivity))/(0.693/HALF_LIFE_PERIOD)
+let result = Math.ceil(res)
 return result
+  }
+  else return false
 }
 
 module.exports = {
